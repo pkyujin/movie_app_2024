5c61bd61cab73fdc63ec46714cc0a833ecd9fee1
pkyujin <luxoris02@sookmyung.ac.kr>
2024-04-13 15:21:59 +0900

    yujin

---
+++ src/TodoListItem.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+export default class TodoListItem extends Component {
+	checkItem() {
+		const checkComponent = document.querySelector("#todo-item" + this.props.id);
+		if (checkComponent.style.cssText) {
+			checkComponent.style.cssText = 0;
+		} else {
+			checkComponent.style.cssText = "text-decoration: line-through";
+		}
+	}
+	render() {
+		return (
+			<li id={"todo-item"+this.props.id}>
+				<input type="button" value="✅" onClick={()=>{this.checkItem()}}/>
+				{this.props.text}
+			</li>
+		)
+	}
+}
